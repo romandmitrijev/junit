f13d317b5d5031a8ffc59e939d82af1a849b7d3e
Roman Dmitrijev <roman@dmitrijev.net>
2019-02-18 21:32:05 +0200

    commit

---
+++ src/main/java/one/Calculator.java
@@ -0,0 +1,12 @@
+package one;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int devide(int a, int b) {
+        return a / b;
+    }
+}
--- src/test/java/one/ExampleTest.java
+++ src/test/java/one/ExampleTest.java
@@ -3,12 +3,34 @@ package one;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertSame;
+
 
 public class ExampleTest {
 
     @Test
-    public void nameMe(){
-        assertTrue(true);
+    public void test(){
+        //given
+        Calculator calculator = new Calculator();
+        //when
+        int result = calculator.add(5,3);
+        //then
+        assert result == 8;
+
     }
 
+    @Test
+    public void testTrue() {
+        assertTrue(5==5);
+
+    }
+
+    @Test
+    public void testObjects() {
+        Integer integer1 = 1;
+        Integer integer2 = 1;
+
+        assertSame(integer1,integer2);
+
+    }
 }
