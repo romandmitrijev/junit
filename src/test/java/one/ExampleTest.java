6d18df2c6e1964b0e492235876acefb63fc6b561
Roman Dmitrijev <roman@dmitrijev.net>
2019-02-18 20:53:17 +0200

    first commit

---
+++ src/test/java/one/ExampleTest.java
@@ -0,0 +1,14 @@
+package one;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class ExampleTest {
+
+    @Test
+    public void nameMe(){
+        assertTrue(true);
+    }
+
+}
